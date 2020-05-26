964ca2f7b031d5787b00a3d9ff92a9d79371646d
Matt Williams <mattinzhuhai@outlook.com>
2020-05-26 14:20:24 -0700

    Added some stuff

--- setup.py
+++ setup.py
@@ -1,21 +1,51 @@
+"""
+When packaging LTF software, as libraires, or applications
+this is the setup.py template that should be used
+"""
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+# constantants that should be front and center
+VERSION = '0.0.1'
+LICENSE = 'BSD 2-Clause'
+PKG_NAME = 'MetalGear'
+REPO_URL = 'https://github.com/mattcoding4days/MetalGear'
+EMAIL = 'mattltf@pm.me'
+
+
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+# much better way to source dependencies, load in
+# requirements.txt, so we only have to maintain the onefile
+# instead of two, and the requirements will be installed automatically
+# when the package is pip installed
+with open('requirements.txt') as depends:
+    requirements = depends.read().splitlines()
+
 setup(
-    name="MetalGear",
-    version="0.0.1",
+    name=PKG_NAME,
+    version=VERSION,
+    install_requires=requirements,
     author="Matt Williams",
-    author_email="mattltf@pm.me",
-    description="Classes that represent foxhound members",
+    author_email=EMAIL,
+    description="Example Package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mattcoding4days/MetalGear",
+    url=REPO_URL,
+    license=LICENSE,
     packages=find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
     ]
 )
